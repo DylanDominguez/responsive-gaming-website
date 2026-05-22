cc2a0cc33b08b8519c9c5e9a7c44b23ecbe40243
DylanDominguez <dylandomperez1802@gmail.com>
2026-05-22 17:10:15 -0500

    Falta solo footer

--- js/script.js
+++ js/script.js
@@ -14,3 +14,44 @@ menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 navLinks.forEach(link => {
   link.addEventListener("click", () => menuOpenButton.click())
 });
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  //Para que cuando el cursor del mouse se acerque a cualquier item, se cambie de flecha a una mano
+  grabCursor: true,
+  //Espacio entre cada elemento del slider
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    //Para que cuando se da click a los botones de abajo del slider, tambien cambie de item
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  /*
+  ? Para que el slider se adapte de manera Resposiva
+  */
+  breakpoints: {
+    //De 0px hasta 767px de ancho
+    0: {
+        slidesPerView: 1
+    },
+    //De 768px hasta 1023px de ancho
+    768: {
+        slidesPerView: 2
+    },
+    //De 1024px de ancho a más
+    1024: {
+        slidesPerView: 3
+    },
+  }
+
+});
